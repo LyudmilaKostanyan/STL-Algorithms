907003c90891135d9e01643b68e4d142e0b482f2
Lyudmila Kostanyan <lkostanyan@griddynamics.com>
2025-03-11 18:48:08 +0400

    Implement my_for_each_n function and update main.cpp to demonstrate its usage

--- main.cpp
+++ main.cpp
@@ -1,12 +1,45 @@
 #include "stl_algorithms.h"
 
+class Sum
+{
+public:
+    void operator()(int value) { sum += value; }
+    int getSum() const { return sum; }
+private:
+    int sum = 0;
+};
+
 int main()
 {
     std::vector<int> arr = {1, 2, 3, 4, 5};
+
+    std::cout << std::endl << "for_each()" << std::endl;
+    std::cout << "Test 1: Functor:" << std::endl;
+    Sum arr_sum = my_for_each(arr.begin(), arr.end(), Sum());
+    std::cout << "Array sum: " << arr_sum.getSum() << std::endl;
+    
+    std::cout << "Test 2: Lambda function: ";
+    std::cout << "Array elements after increment: ";
     my_for_each(arr.begin(), arr.end(), [](int value){
         value++;
-        std::cout << value << std::endl;
+        std::cout << value << " ";
     });
+    std::cout << std::endl;
 
+    std::cout << std::endl << "for_each_n()" << std::endl;
+    std::cout << "Test 1: Functor" << std::endl;
+    Sum arr_sum_n = my_for_each_n(arr.begin(), 3, Sum()).second;
+    std::cout << "Array sum: " << arr_sum_n.getSum() << std::endl;
+
+    std::cout << "Test 2: Lambda function\n";
+    std::cout << "Array elements after increment: ";
+    auto it = my_for_each_n(arr.begin(), 3, [](int value){
+        value++;
+        std::cout << value << " ";
+    });
+    std::cout << std::endl;
+    std::cout << "Remaining elements: ";
+    for (auto i = it.first; i != arr.end(); i++)
+        std::cout << *i << " ";
     return 0;
 }
--- stl_algorithms.h
+++ stl_algorithms.h
@@ -3,6 +3,8 @@
 #include <iterator>
 #include <vector>
 #include <iostream>
+#include <algorithm>
+#include <utility>
 
 template< class InputIt, class UnaryFunc >
 UnaryFunc my_for_each( InputIt first, InputIt last, UnaryFunc f )
@@ -13,4 +15,12 @@ UnaryFunc my_for_each( InputIt first, InputIt last, UnaryFunc f )
         first++;
     }
     return f;
+}
+
+template< class InputIt, class Size, class UnaryFunc >
+std::pair<InputIt, UnaryFunc> my_for_each_n( InputIt first, Size n, UnaryFunc f )
+{
+    for (; n > 0; n--, first++)
+        f(*first);
+    return {first, f};
 }
