e15de9a9a3ffaf93d24c055b5ddef658290c3290
Lyudmila Kostanyan <lkostanyan@griddynamics.com>
2025-03-12 16:41:19 +0400

    Refactor algorithms to use mila namespace and add find_if and find_if_not functions

--- main.cpp
+++ main.cpp
@@ -9,18 +9,22 @@ private:
     int sum = 0;
 };
 
+bool isEven(int n) {
+    return n % 2 == 0;
+}
+
 int main()
 {
     std::vector<int> arr = {1, 2, 3, 4, 5};
 
     std::cout << std::endl << "for_each()" << std::endl;
     std::cout << "Test 1: Functor:" << std::endl;
-    Sum arr_sum = my_for_each(arr.begin(), arr.end(), Sum());
+    Sum arr_sum = mila::for_each(arr.begin(), arr.end(), Sum());
     std::cout << "Array sum: " << arr_sum.getSum() << std::endl;
     
     std::cout << "Test 2: Lambda function: ";
     std::cout << "Array elements after increment: ";
-    my_for_each(arr.begin(), arr.end(), [](int value){
+    mila::for_each(arr.begin(), arr.end(), [](int value){
         value++;
         std::cout << value << " ";
     });
@@ -28,12 +32,12 @@ int main()
 
     std::cout << std::endl << "for_each_n()" << std::endl;
     std::cout << "Test 1: Functor" << std::endl;
-    Sum arr_sum_n = my_for_each_n(arr.begin(), 3, Sum()).second;
+    Sum arr_sum_n = mila::for_each_n(arr.begin(), 3, Sum()).second;
     std::cout << "Array sum: " << arr_sum_n.getSum() << std::endl;
 
     std::cout << "Test 2: Lambda function\n";
     std::cout << "Array elements after increment: ";
-    auto it = my_for_each_n(arr.begin(), 3, [](int value){
+    auto it = mila::for_each_n(arr.begin(), 3, [](int value){
         value++;
         std::cout << value << " ";
     });
@@ -43,11 +47,19 @@ int main()
         std::cout << *i << " ";
 
     std::cout << std::endl << std::endl << "find()" << std::endl;
-    std::cout << "Test 1: Find num in array: ";
-    auto it_find = my_find(arr.begin(), arr.end(), 5);
+    std::cout << "Test 1: Find 5 in array: ";
+    auto it_find = mila::find(arr.begin(), arr.end(), 5);
     if (it_find != arr.end())
-        std::cout << "Found" << std::endl;
+    std::cout << "Found" << std::endl;
     else
-        std::cout << "Not found" << std::endl;
+    std::cout << "Not found" << std::endl;
+    
+    std::cout << std::endl << std::endl << "find_if()" << std::endl;
+    std::cout << "Test 1: Find first even num in array: ";
+    std::cout << *(mila::find_if(arr.begin(), arr.end(), isEven)) << std::endl;
+    std::cout << std::endl << std::endl << "find_if_not()" << std::endl;
+    std::cout << "Test 1: Find first odd num in array: ";
+    std::cout << *(mila::find_if_not(arr.begin(), arr.end(), isEven)) << std::endl;
+    
     return 0;
 }
--- stl_algorithms.h
+++ stl_algorithms.h
@@ -6,30 +6,50 @@
 #include <algorithm>
 #include <utility>
 
-template< class InputIt, class UnaryFunc >
-UnaryFunc my_for_each( InputIt first, InputIt last, UnaryFunc f )
-{
-    while (first != last)
+namespace mila{
+    template< class InputIt, class UnaryFunc >
+    UnaryFunc for_each( InputIt first, InputIt last, UnaryFunc f )
     {
-        f(*first);
-        first++;
+        while (first != last)
+        {
+            f(*first);
+            first++;
+        }
+        return f;
     }
-    return f;
-}
 
-template< class InputIt, class Size, class UnaryFunc >
-std::pair<InputIt, UnaryFunc> my_for_each_n( InputIt first, Size n, UnaryFunc f )
-{
-    for (; n > 0; n--, first++)
-        f(*first);
-    return {first, f};
-}
+    template< class InputIt, class Size, class UnaryFunc >
+    std::pair<InputIt, UnaryFunc> for_each_n( InputIt first, Size n, UnaryFunc f )
+    {
+        for (; n > 0; n--, first++)
+            f(*first);
+        return {first, f};
+    }
 
-template< class InputIt, class T >
-InputIt my_find( InputIt first, InputIt last, const T& value )
-{
-    for (; first != last; first++)
-        if (*first == value)
-            return first;
-    return last;
+    template< class InputIt, class T >
+    InputIt find( InputIt first, InputIt last, const T& value )
+    {
+        for (; first != last; first++)
+            if (*first == value)
+                return first;
+        return last;
+    }
+
+    template< class InputIt, class UnaryPred >
+    InputIt find_if( InputIt first, InputIt last, UnaryPred p )
+    {
+        for (; first != last; first++)
+            if (p(*first))
+                return first;
+        return last;
+    }
+
+    template< class InputIt, class UnaryPred >
+    InputIt find_if_not( InputIt first, InputIt last, UnaryPred q )
+    {
+        for (; first != last; first++)
+            if (!q(*first))
+                return first;
+        return last;
+    }
 }
